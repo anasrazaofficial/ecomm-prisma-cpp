6c70900641095a921ca7e2eaabada2aac44e7a2a
anasrazaofficial <anasbukhari4114@gmail.com>
2024-12-22 02:06:29 +0500

    A little bit more

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 - `npx prisma migrate dev --name init` = Migrates or pushes changes to database. [Link](npx prisma migrate dev --name init0)
 - `npm i @prisma/client` = Installs prisma client
 - `npx prisma generate` = Generates prisma client code
+- `foreignKey Table @relation(fields: [foreignKey], refereneces: [tablePK])`
--- script.js
+++ script.js
@@ -2,8 +2,9 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-    const user = await prisma.user.create({ data: { username: "anas", password: "abc123" } });
-    console.log(user);
+    // const user = await prisma.user.create({ data: { username: "anas", password: "abc123" } });
+    const users = await prisma.user.findMany();
+    console.log(users);
 }
 
 main().catch((e) => console.error(e.message)).finally(async () => await prisma.$disconnect());

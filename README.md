1f9b447d58d374ad8067313ac3b2891af56cbad0
anasrazaofficial <anasbukhari4114@gmail.com>
2024-12-22 01:25:34 +0500

    Learn how to connect and post

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ecomm-prisma-cpp
+
+- `npx prisma migrate dev --name init` = Migrates or pushes changes to database. [Link](npx prisma migrate dev --name init0)
+- `npm i @prisma/client` = Installs prisma client
+- `npx prisma generate` = Generates prisma client code
---
+++ script.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+    const user = await prisma.user.create({ data: { username: "anas", password: "abc123" } });
+    console.log(user);
+}
+
+main().catch((e) => console.error(e.message)).finally(async () => await prisma.$disconnect());
